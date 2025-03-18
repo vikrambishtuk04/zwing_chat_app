627e32b738d52e81aa97866c1d1abc248b3fac2c
Vikram Bisht <vikram.b@gsl.in>
2025-03-18 13:08:14 +0530

    integrate mysql db and created a User list api

--- src/modules/users/controller.ts
+++ src/modules/users/controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './service';
-import { CreateUserDto } from './dto/CreateUserDto';
 
 @Controller('users')
 export class UserController {
@@ -8,17 +7,5 @@ export class UserController {
   @Get('/list')
   userList() {
     return this.userService.getUserList();
-  }
-  @Post('/create')
-  userCreate(@Body() CreateUserDto: CreateUserDto) {
-    return this.userService.create(CreateUserDto);
-  }
-  @Get('/update')
-  userUpdate() {
-    return 'User update underconstruction';
-  }
-  @Get('/delete')
-  userDelete() {
-    return 'User delete underconstruction';
   }
 }
--- src/modules/users/service.ts
+++ src/modules/users/service.ts
@@ -1,46 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { User } from './schema';
-import { Model } from 'mongoose';
-import { CreateUserDto } from './dto/CreateUserDto';
 import { ResponseDto } from 'src/common/dto/ResponeDto';
-
+import { getDb } from 'db/config';
+import { vendorUsers, VendorUser, vendorRoles, vendorRoleUserMapping } from 'db/schema';
+import { eq } from 'drizzle-orm';
+import { UserListResponse } from './dto/UserResponseDto';
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  async create(createUserDto: CreateUserDto): Promise<ResponseDto<User>> {
-    //dummy user create
-    // const user = {
-    //   name: 'Shushank Gautam',
-    //   mobile: '1111122222',
-    //   email: 'gautam123@gmai.com',
-    //   username: 'ShushankTestAudit',
-    //   store_allocation_type: '1',
-    //   role: 'cashier',
-    //   allocateStore: 'PLUM Thane',
-    //   status: '1',
-    //   vu_id: 4638,
-    //   createdat: '2025-03-05 09:43:48',
-    //   updatedat: '2025-03-05 09:43:48',
-    // };
-
-    const createdUser = new this.userModel(createUserDto);
-    //if the method directly returns the createdUser.save() then no nee to use await as nest will
-    //  handle it automatically but since we are return a string we need to use await on save
-    await createdUser.save();
-    return new ResponseDto<User>(
-      'success',
-      'user added successfully',
-      createdUser,
-    );
-  }
-  async getUserList(): Promise<ResponseDto<User[]>> {
-    const users = await this.userModel.find().exec();
-    return new ResponseDto<User[]>(
+  async getUserList(): Promise<ResponseDto<UserListResponse[]>> {
+    const db = await getDb();
+    const userList = await db.select({
+      id: vendorUsers.id,
+      name: vendorUsers.first_name,
+      status: vendorUsers.status,
+      email: vendorUsers.email,
+      role_name: vendorRoles.name,
+    })
+    .from(vendorUsers)
+    .leftJoin(
+      vendorRoleUserMapping,
+      eq(vendorUsers.id, vendorRoleUserMapping.user_id)
+    )
+    .leftJoin(
+      vendorRoles,
+      eq(vendorRoleUserMapping.role_id, vendorRoles.id)
+    )
+    .where(eq(vendorUsers.status, '1'));
+    return new ResponseDto<UserListResponse[]>(
       'success',
       'users fetched successfully',
-      users,
+      userList,
     );
   }
 }
