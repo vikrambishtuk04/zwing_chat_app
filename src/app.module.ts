b4f9cee254d8d997e318e2e5f7a1e733c7eeb093
Vikram Bisht <vikram.b@gsl.in>
2025-03-19 22:32:48 +0530

    conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './modules/users/module';
 import { ChatModule } from './modules/chats/module';
+import { FileModule } from './common/modules/file.modules';
+
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/zwing_chat'),
     UserModule,
     ChatModule,
+    FileModule,
   ],
   controllers: [],
   providers: [],
--- src/modules/chats/controllers/chat.controller.ts
+++ src/modules/chats/controllers/chat.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';   
 import { ChatService } from '../services/chat.service';
-
+import { fileUploadHelper } from '../../../common/helper/file.upload';
+import { FileService } from '../../../common/service/file.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { BadRequestException } from '@nestjs/common';
 @Controller('chats')
 export class ChatController {
-  constructor(private readonly chatService: ChatService) {}
+  constructor(private readonly chatService: ChatService, private readonly fileService: FileService) {}
 
   @Get('/list')
   chatList() {
     return 'Chat Service Running';
+  }
+
+  @Post('/upload')
+  @UseInterceptors(FileInterceptor('file', fileUploadHelper))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      console.log('No file received');
+      throw new BadRequestException('No file uploaded');
+    }
+    // console.log('File received:', file);
+    // return { filePath: file.path };
+    return { status: true, message: 'File uploaded successfully', filePath: this.fileService.getFilePath(file) };
   }
 }
